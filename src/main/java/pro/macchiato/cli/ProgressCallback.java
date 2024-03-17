c650511a2f0e174db63ecef04ccd5f2cec4324d3
Devin Catuns <devincatuns1@gmail.com>
2024-03-17 15:43:36 -0400

    add: TransCli tests

--- src/main/java/pro/macchiato/cli/ProgressCallback.java
+++ src/main/java/pro/macchiato/cli/ProgressCallback.java
@@ -3,4 +3,10 @@ package pro.macchiato.cli;
 public interface ProgressCallback {
 
     void processLine(String line);
+
+    /**
+     * Time elapsed in seconds
+     * @return seconds
+     */
+    long getElapsed();
 }
